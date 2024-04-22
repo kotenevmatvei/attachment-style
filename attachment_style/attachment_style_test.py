e06614c9c1f8ff0a5b370fde5ea2d6ce97ef4480
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-04-22 09:47:12 +0200

    Read txt's and fill the question lists

--- attachment_style/attachment_style_test.py
+++ attachment_style/attachment_style_test.py
@@ -1,7 +1,19 @@
 # Imports
 import matplotlib.pyplot as plt
 
-# Build the lists with questions
+# Read the txt files with questions and build corresponding lists
+anxious_questions = []
+secure_questions = []
+avoidant_questions = []
+with open("anxious_questions.txt", "r") as f:
+    for i in range(13):
+        anxious_questions.append(f.readline()[:-1]) # Cut off the \n at the end
+with open("secure_questions.txt", "r") as f:
+    for i in range(13):
+        secure_questions.append(f.readline()[:-1])
+with open("avoidant_questions.txt", "r") as f:
+    for i in range(13):
+        avoidant_questions.append(f.readline()[:-1])
 
 
 # Setup the lists to store the results
