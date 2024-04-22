582fd1563ee4af87c7a90391f3052a74348971a1
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-04-22 13:04:03 +0200

    Implemented anwers between 0 and 1. todo: 1 to 10

--- attachment_style/attachment_style_test.py
+++ attachment_style/attachment_style_test.py
@@ -23,58 +23,77 @@ list_same_length = (
     len(anxious_questions) == len(secure_questions) == len(avoidant_questions)
 )
 if not list_same_length:
-    print(len(anxious_questions), len(secure_questions), len(avoidant_questions))
     sys.exit("Lists with questions must be the same length")
 
 # Setup the lists to store the results
 anxious_results: dict[str, int] = {}
 secure_results: dict[str, int] = {}
-avoidant_results: dict[str, int] ={}
+avoidant_results: dict[str, int] = {}
 
 # Explain the rules
 print(
-    "Answer the following questions by entering 1 for yes and 0 for 1.\n"
+    "\nAnswer the following questions by entering a number between 0 and 1 "
+    "indicating the extent to each statement applies to you. Please use . as "
+    "the decimal separator. For example, enter 0.8 if the statement is true of "
+    "you in most situations, but not always\n"
 )
 
+
 # Ask questions and store the results
-def collect_answers(questions: list[str]) -> dict[str, int]:
-    answers: dict[str, int] = {}
+def collect_answers(questions: list[str]) -> dict[str, float]:
+    answers: dict[str, float] = {}
     for question in questions:
         input_valid = False
         while input_valid == False:
-            answer = input(question)
-            if answer == "0" or answer == "1":
-                input_valid = True
-                answers[question] = int(answer)
-            else:
-                print("Invalid input. Please enter 1 for yes and 0 for no")
+            try:
+                answer = float(input(f"{question}:  "))
+                if answer >= 0 and answer <= 1:
+                    input_valid = True
+                    answers[question] = float(answer)
+                else:
+                    print(
+                        "Invalid input, the answer must greater than or equal "
+                        "to 0 and less than or equal to 1."
+                    )
+            except ValueError:
+                print("Invalid input. Please enter a number.")
     return answers
 
-anxious_results: dict[str, int] = collect_answers(anxious_questions)
-secure_results: dict[str, int] = collect_answers(secure_questions)
-avoidant_results: dict[str, int] = collect_answers(avoidant_questions)
+
+anxious_results: dict[str, float] = collect_answers(anxious_questions)
+secure_results: dict[str, float] = collect_answers(secure_questions)
+avoidant_results: dict[str, float] = collect_answers(avoidant_questions)
 
 # Calculate the score
-anxious_score: int = sum(anxious_results.values())
-secure_score: int = sum(secure_results.values())
-avoidant_score: int = sum(avoidant_results.values())
+anxious_score: float = sum(anxious_results.values())
+secure_score: float = sum(secure_results.values())
+avoidant_score: float = sum(avoidant_results.values())
 
 # Build the plot
-n: int = len(anxious_questions) # length of the scala
+n: int = len(anxious_questions)  # length of the scala
 fig, ax = plt.subplots()  # Create a figure containing a single axes
-plt.xlim(0,14)
-plt.ylim(0,14)
+plt.xlim(0, 14)
+plt.ylim(0, 14)
 plt.grid(True)
-ax.set_xticks(list(range(0,15)))
-ax.set_yticks(list(range(0,15)))
-plt.axhline(7, color='brown', linewidth=1)
-plt.axvline(7, color='brown', linewidth=1) 
+ax.set_xticks(list(range(0, 15)))
+ax.set_yticks(list(range(0, 15)))
+plt.axhline(7, color="brown", linewidth=1)
+plt.axvline(7, color="brown", linewidth=1)
 ax.scatter(anxious_score, avoidant_score)
 plt.gca().invert_yaxis()
-plt.tick_params(axis='x', which='both', bottom=False, top=True, 
-                labelbottom=False, labeltop=True)
-plt.savefig('plot0.png')
+plt.tick_params(
+    axis="x",
+    which="both",
+    bottom=False,
+    top=True,
+    labelbottom=False,
+    labeltop=True,
+)
+plt.savefig("plot0.png")
 plt.xlabel("Anxiety")
 plt.xlabel("Avoidance")
 plt.title("Attachment Style Score")
 plt.show()
+
+# TODO Delete periods
+# TODO scale 1 to 10
