8687315f4cb027624970b87cf4a640f42fb854f4
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-04-22 10:28:46 +0200

    Added type hints, turned results to dictionaries, implemented results collection with type checking

--- attachment_style/attachment_style_test.py
+++ attachment_style/attachment_style_test.py
@@ -2,9 +2,10 @@
 import matplotlib.pyplot as plt
 
 # Read the txt files with questions and build corresponding lists
-anxious_questions = []
-secure_questions = []
-avoidant_questions = []
+anxious_questions: list[str] = []
+secure_questions: list[str] = []
+avoidant_questions: list[str] = []
+
 with open("anxious_questions.txt", "r") as f:
     for i in range(13):
         anxious_questions.append(f.readline()[:-1]) # Cut off the \n at the end
@@ -17,9 +18,9 @@ with open("avoidant_questions.txt", "r") as f:
 
 
 # Setup the lists to store the results
-anxious_results = [0]*14
-secure_results = [0]*14
-avoidant_results =[0]*14
+anxious_results: dict[str, int] = {}
+secure_results: dict[str, int] = {}
+avoidant_results: dict[str, int] ={}
 
 # Explain the rules
 print(
@@ -27,6 +28,16 @@ print(
 )
 
 # Ask questions and store the results
-
+for question in anxious_questions:
+    input_valid = False
+    while input_valid == False:
+        answer = input(question)
+        if answer == "0" or answer == "1":
+            input_valid = True
+            anxious_results[question] = int(answer)
+        else:
+            print("Invalid input. Please enter 1 for yes and 0 for no")
+    
+0
 
 # Visualize
