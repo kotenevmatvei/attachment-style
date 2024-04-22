00498ef37b7427324f48787b7aee99812ef65800
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-04-22 09:32:17 +0200

    Added txt files with questions, general structure (comments)

---
+++ attachment_style/attachment_style_test.py
@@ -0,0 +1,15 @@
+# Imports
+import matplotlib.pyplot as plt
+
+# Build the lists with questions
+
+
+# Setup the lists to store the results
+anxious_results = [0]*14
+secure_results = [0]*14
+avoidant_results =[0]*14
+
+# Ask questions and store the results
+
+
+# Visualize
