4526b7a19a36308f8340b83f4f89d2764d50cc5a
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-06-11 01:18:05 +0200

    hidden text working

--- attachment_style/dash_mantine_app.py
+++ attachment_style/dash_mantine_app.py
@@ -1,15 +1,24 @@
 import dash_mantine_components as dmc
-from dash import Dash
+from dash import Dash, Input, Output, State, html
 
 app = Dash(__name__)
 
 app.layout = dmc.MantineProvider(
-    dmc.Alert(
-       "Hi from Dash Mantine Components. You can create some great looking dashboards using me!",
-       title="Welcome!",
-       color="violet",
-    )
+    children = [dmc.Button("toggle button", id='toggle-button'),
+    dmc.Text(id='my-text', children='This is some text'),
+    dmc.Text("More Text yoo")
+    ]
 )
 
+@app.callback(
+    Output('my-text', 'style'),
+    Input('toggle-button', 'n_clicks'),
+)
+def toggle_text(n_clicks):
+    if n_clicks is None or n_clicks % 2 == 0:
+        return {'display': 'block'}  # Show the text
+    else:
+        return {'display': 'none'}  # Hide the text
+
 if __name__ == "__main__":
-    app.run_server()
+    app.run_server(debug=True)
