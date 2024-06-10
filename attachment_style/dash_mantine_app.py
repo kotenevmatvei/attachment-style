5ca5e81b4ae52e03706da313ff75d8005b0b20d1
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-06-10 13:29:29 +0200

    test mantine app working

---
+++ attachment_style/dash_mantine_app.py
@@ -0,0 +1,15 @@
+import dash_mantine_components as dmc
+from dash import Dash
+
+app = Dash(__name__)
+
+app.layout = dmc.MantineProvider(
+    dmc.Alert(
+       "Hi from Dash Mantine Components. You can create some great looking dashboards using me!",
+       title="Welcome!",
+       color="violet",
+    )
+)
+
+if __name__ == "__main__":
+    app.run_server()
