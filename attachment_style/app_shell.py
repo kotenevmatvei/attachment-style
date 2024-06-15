d817cbce6db90f65b17a56b658f5e579dedd8fe3
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-06-15 16:31:55 +0200

    grid col structure

--- attachment_style/app_shell.py
+++ attachment_style/app_shell.py
@@ -6,15 +6,50 @@ app = Dash(__name__)
 app.layout = dmc.MantineProvider(
     dmc.AppShell(
         [
-            dmc.AppShellHeader("Header"),
-            dmc.AppShellMain("Main"),
+            dmc.AppShellHeader(
+                # dmc.Title(
+                #     "Attachment Style Test",
+                #     styles={"align": "center"},
+                # )
+                dmc.Grid(
+                    children=[
+                        dmc.GridCol(
+                            dmc.Title(
+                                "Attachment Style Test",
+                                ta="center",
+                                mt=5,
+                            ),
+                            span=4,
+                            offset=4
+                        ),
+                        dmc.GridCol(
+                            dmc.Button(
+                                "Language",
+                                ta="center",
+                                mt=10
+                            ),
+                            span=1,
+                            offset=2
+                        )
+                    ],
+                    # justify="center",
+                    # styles={
+                    #     "justify-content": "center"
+                    # }
+                ),
+            ),
+            dmc.AppShellMain(
+                dmc.Center(
+                    dmc.Card("Am I at the center?")
+                )
+            ),
             dmc.AppShellFooter("Footer")
         ],
         header={
-            "height": 70,
+            "height": 60,
         },
         footer={
-            "height": 70
+            "height": 60
         }
     )
 )
