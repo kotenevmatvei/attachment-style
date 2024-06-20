09a1ed4353fe9485730e854ea99733520939c658
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-06-20 20:41:41 +0200

    cleanup

--- attachment_style/components/question_card.py
+++ attachment_style/components/question_card.py
@@ -7,9 +7,9 @@ QuestionCard = dbc.Row(
             children=[
                 dbc.CardHeader(
                     [
-                        dbc.Button(className="bi bi-arrow-left-circle d-flex align-items-center"),
-                        dcc.Markdown("##### Question 1/42", className="mx-2 px-3 pt-1 border border-3 rounded"),
-                        dbc.Button(className="bi bi-arrow-right-circle d-flex align-items-center"),
+                        dbc.Button(id="left-button", className="bi bi-arrow-left-circle d-flex align-items-center"),
+                        dcc.Markdown("##### Question 1/42", id="question-count-text", className="mx-2 px-3 pt-1 border border-3 rounded"),
+                        dbc.Button(id="right-button", className="bi bi-arrow-right-circle d-flex align-items-center"),
                     ],
                     className="d-flex align-items-center justify-content-center",
                     style={"height": "3.5rem"}
@@ -18,9 +18,19 @@ QuestionCard = dbc.Row(
                     dcc.Markdown("Text of the question", id="question-text")
                 ),
                 dbc.CardFooter(
-                    dcc.Slider(min=0, max=10, step=.1, value=5, marks=None, tooltip={"placement": "bottom", "always_visible": False}),
-                    style={"height": "2rem"}
-                )
+                    dcc.Slider(
+                        id="slider",
+                        min=0,
+                        max=10,
+                        step=.1,
+                        value=0,
+                        marks=None,
+                        tooltip={
+                            "placement": "bottom",
+                            "always_visible": False,
+                        }
+                    ),
+                ),
             ],
             style={"width": "45rem"},
             className="border text-center"
