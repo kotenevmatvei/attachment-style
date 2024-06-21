c4264b9d57252a46d75f325ce818b58d310ec8f0
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-06-21 13:31:43 +0200

    Right button + answers

--- attachment_style/app.py
+++ attachment_style/app.py
@@ -20,7 +20,7 @@ app.layout = html.Div([
     # storage
     dcc.Store(id="questions-storage", data=read_questions(), storage_type="session"),
     dcc.Store(id="question-count-storage", data=0),
-    dcc.Store(id="answers-storage"),
+    dcc.Store(id="answers-storage", data={}),
     dcc.Store(id="last-question-visited-flag")
 ])
 
@@ -29,6 +29,7 @@ app.layout = html.Div([
         Output("question-count-storage", "data"),
         Output("question-count-text", "children"),
         Output("question-text", "children"),
+        Output("answers-storage", "data")
     ],
     [
         Input("right-button", "n_clicks")
@@ -37,31 +38,39 @@ app.layout = html.Div([
         State("question-count-storage", "data"),
         State("questions-storage", "data"),
         State("answers-storage", "data"),
+        State("slider", "value")
     ]
 )
-def update_question(r_clicks, question_count, questions, answers):
+def update_question(r_clicks, question_count, questions, answers, slider_value):
     n = len(questions)
     # first question / inital state
     if r_clicks is None:
         return (
             1,
             f"Question {1}/{n}",
-            questions[0][0]
+            questions[0][0],
+            answers
         )
     # questions between first and last
     elif question_count < n:
+        answers[question_count-1] = slider_value
+        print(question_count-1, answers[question_count-1])
         question_count += 1
         return (
             question_count,
             f"Question {question_count}/{n}",
-            questions[question_count-1][0]
+            questions[question_count-1][0],
+            answers
         )
     # last question
     else:
+        answers[question_count-1] = slider_value
+        print(question_count - 1, answers[question_count - 1])
         return (
             question_count,
             f"Question {n}/{n}",
             questions[n-1][0],
+            answers
         )
 
 if __name__ == "__main__":
