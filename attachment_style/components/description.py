a1ccd185e6e912ec8c4c35ad072976bd7620368f
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-07-15 16:35:38 +0200

    description edit

--- attachment_style/components/description.py
+++ attachment_style/components/description.py
@@ -5,7 +5,8 @@ Description = dbc.Container(
     dcc.Markdown("""
     #### Welcome to the attachment style test!
     Please evaluate how much you can relate to the following statements on the scale from 0 to 10.\n
-    Test Your Partner page offers more behaviour-based questions that you can answer with regard to someone you know (as well as yourself).
+    Test Your Partner page offers more behaviour-based questions with examples that you can answer with regard to someone you know (as well as yourself).
     """),
+    style={"width": "80%"},
     className="mb-4 text-center"
 )
