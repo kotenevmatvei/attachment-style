a4379250ce0973a6b4eda4af2a1dfab49c9c4b67
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-07-15 16:54:33 +0200

    description edit

--- attachment_style/components/description.py
+++ attachment_style/components/description.py
@@ -7,6 +7,5 @@ Description = dbc.Container(
     Please evaluate how much you can relate to the following statements on the scale from 0 to 10.\n
     Test Your Partner page offers more behaviour-based questions with examples that you can answer with regard to someone you know (as well as yourself).
     """),
-    style={"width": "80%"},
     className="mb-4 text-center"
 )
