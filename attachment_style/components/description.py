62ab38a2cc211aa8a24943a6f76f191a9e026360
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-07-15 02:24:17 +0200

    adapted navbar and description

--- attachment_style/components/description.py
+++ attachment_style/components/description.py
@@ -4,7 +4,8 @@ from dash import dcc
 Description = dbc.Container(
     dcc.Markdown("""
     #### Welcome to the attachment style test!
-    Please evaluate how much you can relate to the following statements on the scale from 1 to 10.
+    Please evaluate how much you can relate to the following statements on the scale from 0 to 10.  
+    Test Your Partner page offers more behaviour-based questions that you can answer with regard to someone you know (or you as well).
     """),
     className="mb-4 text-center"
 )
