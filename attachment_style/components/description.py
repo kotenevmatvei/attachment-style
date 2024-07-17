2a90ef935241c9d149f5e2f145c8b5c778f647ac
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-07-17 21:56:34 +0200

    Updated description

--- attachment_style/components/description.py
+++ attachment_style/components/description.py
@@ -12,11 +12,12 @@ Description = html.Div(
             ),
             dbc.Row(
                 dbc.Col(
-                    dcc.Markdown("""
-                    Please evaluate how much you can relate to the following statements on the scale from 0 to 10.\n 
-                    If you are not currently in a relationship, you can remember how you acted in your previous relationships
-                    or imagine how you would act if you were in a relationship.
-                    """),
+                    dcc.Markdown(
+                        """
+                        Please evaluate how much you can relate to the following statements on the scale from 0 to 10.
+                        """,
+                        className="text-center"
+                    ),
                     width=12,
                     sm=8,
                     className="mb-4 mx-auto"
