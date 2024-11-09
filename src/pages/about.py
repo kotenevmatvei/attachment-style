e7d57987b6df5eeed32a4a59af0a9a7207d8431a
Matvei Kotenev <matvei.kotenev@hh2e.de>
2024-11-09 14:46:07 +0100

    fix:typo

--- src/pages/about.py
+++ src/pages/about.py
@@ -13,7 +13,7 @@ def layout(**kwargs):
             The main advantages of this quiz compared to others that may be found on the internet are:
             
             - The opportunity to download a pdf of the final report with all the questions and answers. It can be
-            used a conversation starter for someone struggling in a relationship.
+            used as a conversation starter for someone struggling in a relationship.
             - The option to be more differentiated in your answers - on the scale from 0 to 10 instead of the standard
             "strongly disagree" to "neutral" to "strongly agree".
             - A more practical, behaviour-based questionnaire with examples that can be completed with regard to someone 
