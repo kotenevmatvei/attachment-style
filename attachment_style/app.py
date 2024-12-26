e03529bf4b696fabb9c140b90f06bac84f084b4f
Matvei Kotenev <kotenev.matvei@gmail.com>
2024-12-26 19:11:20 +0100

    add footer

--- attachment_style/app.py
+++ attachment_style/app.py
@@ -1,8 +1,9 @@
-from dash import Dash, page_container
+from dash import Dash, page_container, html
 
 import dash_bootstrap_components as dbc
 
 from components.navbar import Navbar
+from components.footer import Footer
 
 app = Dash(
     __name__,
@@ -11,8 +12,37 @@ app = Dash(
 )
 server = app.server
 
-app.layout = dbc.Container([Navbar, page_container])
+app.layout = dbc.Container(
+    [
+        Navbar,
+        dbc.Row(
+            dbc.Col(
+                page_container,
+            ),
+            style={"flex": "1"},
+        ),
+        dbc.Row(
+            dbc.Col(
+                html.Footer(
+                    "Created by Matvei Kotenev. Contact via kotenev.matvei@gmail.com.",
+                    style={"margin": "0"}
+                )
+            ),
+            style={
+                "backgroundColor": "#f1f1f1",
+                "position": "sticky",
+                "textAlign": "center",
+                "padding": "10px"
+            },
+        ),
+    ],
+    style={
+        "display": "flex",
+        "flexDirection": "column",
+        "height": "100vh",
+    },
+)
 
 if __name__ == "__main__":
-	# app.run_server(host="0.0.0.0", port=8050)
-	app.run(debug=True)
+    # app.run_server(host="0.0.0.0", port=8050)
+    app.run(debug=True)
