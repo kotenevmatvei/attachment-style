547e89d815998524c0c780a3fc178587339a3fba
Matvei Kotenev <kotenev.matvei@gmail.com>
2025-01-05 15:50:16 +0100

    log when done with data fetch

--- attachment_style/pages/dashboard.py
+++ attachment_style/pages/dashboard.py
@@ -63,6 +63,7 @@ def layout(**kwargs):
                 id="include_test_data",
                 className="mb-2",
             ),
+            html.Div(id="log-div"),
             dbc.Row(
                 children=[
                     dbc.Col(
@@ -376,7 +377,7 @@ def layout(**kwargs):
 
 # load the data from db
 @callback(
-    Output("data-store", "data"),
+    [Output("data-store", "data"), Output("log-div", "children")],
     Input("include_test_data", "value"),
 )
 def include_test_data(include_test_data):
@@ -384,11 +385,11 @@ def include_test_data(include_test_data):
         df1, df2 = get_data_from_db(test=True)
         df1, df2 = aggregate_scores(df1, df2)
         answers_dict = df1.to_dict()
-        return answers_dict
+        return answers_dict, "we're done"
     df1, df2 = get_data_from_db(test=False)
     df1, df2 = aggregate_scores(df1, df2)
     answers_dict = df1.to_dict()
-    return answers_dict
+    return answers_dict, "we're done"
 
 
 # BOX PLOT
