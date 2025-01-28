c393d455f09c4dc5ad82101299b373b5a1809bb7
Matvei Kotenev <kotenev.matvei@gmail.com>
2025-01-28 20:03:58 +0100

    moved question bttons to footer

--- attachment_style/components/question_card.py
+++ attachment_style/components/question_card.py
@@ -7,18 +7,10 @@ QuestionCard = dbc.Row(
             children=[
                 dbc.CardHeader(
                     [
-                        dbc.Button(
-                            id="left-button",
-                            className="bi bi-arrow-left-circle d-flex align-items-center",
-                        ),
                         dcc.Markdown(
                             "##### Question 1/42",
                             id="question-count-text",
                             className="mx-2 px-3 pt-1 border border-3 rounded",
-                        ),
-                        dbc.Button(
-                            id="right-button",
-                            className="bi bi-arrow-right-circle d-flex align-items-center",
                         ),
                     ],
                     className="d-flex align-items-center justify-content-center",
@@ -26,33 +18,44 @@ QuestionCard = dbc.Row(
                 ),
                 dbc.CardBody(dcc.Markdown("Text of the question", id="question-text")),
                 dbc.CardFooter(
-                    dbc.Container(
-                        dcc.Slider(
-                            id="slider",
-                            min=0,
-                            max=10,
-                            step=1,
-                            value=0,
-                            marks={
-                                0: "0",
-                                1: "1",
-                                2: "2",
-                                3: "3",
-                                4: "4",
-                                5: "5",
-                                6: "6",
-                                7: "7",
-                                8: "8",
-                                9: "9",
-                                10: "10",
-                            },
-                            tooltip={
-                                "placement": "bottom",
-                                "always_visible": True,
-                            },
+                    [
+                        dbc.Button(
+                            id="left-button",
+                            className="bi bi-arrow-left-circle d-flex align-items-center",
                         ),
-                        style={"width": "90%"},
-                    )
+                        dbc.Container(
+                            dcc.Slider(
+                                id="slider",
+                                min=0,
+                                max=10,
+                                step=1,
+                                value=0,
+                                marks={
+                                    0: "0",
+                                    1: "1",
+                                    2: "2",
+                                    3: "3",
+                                    4: "4",
+                                    5: "5",
+                                    6: "6",
+                                    7: "7",
+                                    8: "8",
+                                    9: "9",
+                                    10: "10",
+                                },
+                                tooltip={
+                                    "placement": "bottom",
+                                    "always_visible": True,
+                                },
+                            ),
+                            style={"width": "90%"},
+                        ),
+                        dbc.Button(
+                            id="right-button",
+                            className="bi bi-arrow-right-circle d-flex align-items-center",
+                        ),
+                    ],
+                    style={"display": "flex"}
                 ),
             ],
             style={"width": "45rem"},
