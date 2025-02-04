ca76edfe751afc14f8c3f4327196607c0e56fb16
Matvei Kotenev <kotenev.matvei@gmail.com>
2025-02-04 17:49:49 +0100

    port

--- attachment_style/app.py
+++ attachment_style/app.py
@@ -213,5 +213,5 @@ app.clientside_callback(
 )
 
 if __name__ == "__main__":
-    app.run_server(host="0.0.0.0", port=8050)
-    # app.run(debug=True)
+    # app.run_server(host="0.0.0.0", port=8050)
+    app.run(debug=True)
